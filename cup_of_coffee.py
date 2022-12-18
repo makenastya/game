63a933707cbe1d42fba0c43e5dd2bfa5acc92344
kalininatt <kalinina.tt@phystech.edu>
2022-12-18 21:34:56 +0300

    + cup of coffee

---
+++ cup_of_coffee.py
@@ -0,0 +1,6 @@
+import pygame
+##
+image = pygame.image.load("cup_of_tea.png")
+image = pygame.transform.scale(image, (50, 50))
+image.set_colorkey((255, 255, 255))
+screen.blit(image, (#координаты вставки))
